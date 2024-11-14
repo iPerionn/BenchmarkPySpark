606e3df76dc0535fa2205727b6cef0adcdeebfd0
Henri Cossais <henri.cossais@etu.univ-nantes.fr>
2024-11-14 10:50:19 +0000

    sql algo

---
+++ pagerank_the_final_sql.py
@@ -0,0 +1,94 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, lit, when, count
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, DoubleType
+
+import time
+import sys
+from datetime import datetime
+
+output_path = "gs://benchmark_output/pagerank_results"
+
+# Créez une session Spark
+spark = SparkSession.builder\
+    .appName("MonAppSpark")\
+    .master("local[*]")\
+    .config("spark.executor.memory", "12g")\
+    .config("spark.executor.cores", "4")\
+    .config("spark.driver.memory", "12g")\
+    .config("spark.driver.cores", "1")\
+    .config("spark.local.dir", "/tmp/spark-tmp")\
+    .getOrCreate()
+
+schema = StructType([
+    StructField("source", StringType(), nullable=True),
+    StructField("predicate", StringType(), nullable=True),
+    StructField("target", StringType(), nullable=True)
+])
+
+# Chargez vos données web en tant que DataFrame
+#data = spark.read.option("delimiter"," ").csv("gs://public_lddm_data/page_links_en.nt.bz2", header=False, schema=schema)
+data = spark.read.option("delimiter"," ").csv("gs://public_lddm_data/small_page_links.nt", header=False, schema=schema)
+
+# Calculer le nombre de liens sortants par page
+outgoing_links = data.groupBy("source").agg(count("target").alias("outDegree"))
+
+# Récupérer toutes les pages distinctes
+pages_from_source = data.select("source").distinct()
+pages_from_target = data.select("target").distinct()
+
+# Créer un DataFrame avec toutes les pages distinctes
+all_pages = pages_from_source.union(pages_from_target).distinct()
+
+# Joindre pour obtenir le nombre de liens sortants, en remplaçant les valeurs nulles par 0
+pagerank = all_pages.join(outgoing_links, all_pages.source == outgoing_links.source, "left") \
+                  .select(all_pages.source.alias("page"), col("outDegree")) \
+                  .fillna(0, subset=["OutDegree"])
+
+# Ajouter une colonne pagerank initialisée à 1
+pagerank = pagerank.withColumn("pagerank", lit(1.0).cast(DoubleType()))
+
+iteration = 10
+
+# Facteur de dumping
+d = 0.85
+
+debut1 = time.time()
+
+for i in range(iteration):
+  links_with_pagerank = data.join(pagerank, data.source == pagerank.page)\
+    .select("target", "page", "pagerank", "outDegree")
+
+  # Calculer la contribution de chaque page
+  contributions = links_with_pagerank.withColumn("contribution", col("pagerank") / col("OutDegree").cast(DoubleType()))
+
+  # Somme des contributions pour chaque page cible
+  rank_updates = contributions.groupBy("target").agg(F.sum("contribution").alias("sum_contributions"))
+
+  # Calculer le nouveau PageRank en utilisant le facteur de dumping
+  result = pagerank.join(rank_updates, pagerank.page == rank_updates.target, "left") \
+                  .withColumn("sum_contributions", when(col("sum_contributions").isNull(), 0).otherwise(col("sum_contributions")).cast(DoubleType())) \
+                  .withColumn("pagerank", ((1 - d) + d * col("sum_contributions")).cast(DoubleType())) \
+                  .select("page", "outDegree", "pagerank")
+
+result.collect()
+
+fin1 = time.time()
+
+max_pagerank_page = result.orderBy(col("pagerank"), ascending=False).limit(1).collect()[0]
+
+# Enregistrement des résultats dans un format de chaîne
+output_data = (
+    f"Temps d'exécution : {fin1-debut1} secondes\n"
+    f"Link with the highest PageRank: {max_pagerank_page['page']}, PageRank: {max_pagerank_page['pagerank']}\n"
+)
+
+# Generate a unique timestamp
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+# Define the output path using the timestamp
+output_file = f"gs://benchmark_output/pagerank_results/SQL_Singlenode_output_folder_{timestamp}"
+
+# Sauvegarder les résultats directement dans GCS
+sc = spark.sparkContext
+sc.parallelize([output_data]).saveAsTextFile(output_file)
--- run_pagerank.sh
+++ run_pagerank.sh
@@ -4,26 +4,26 @@
 PROJECT_ID="donneedistribuees"   # Remplacez par votre Project ID GCP
 REGION="us-central1"             # Modifiez selon votre région préférée
 REPO_URL="https://github.com/iPerionn/BenchmarkPySpark"  # URL du dépôt GitHub
-SCRIPT_NAME="pagerank_an_pyspark_rdd.py"      # Nom du script à exécuter
+SCRIPT_NAME="pagerank_the_final_sql.py"      # Nom du script à exécuter
 INPUT_DATA="gs://public_lddm_data/"  # Lien vers les données
 OUTPUT_BUCKET="gs://benchmark_output"  # Remplacez par votre bucket GCS
 
 gsutil cp ./${SCRIPT_NAME} gs://benchmark_output/ 
 
 # Définir le nom du cluster pour cette exécution
-CLUSTER_NAME="cluster-quadnode"
+CLUSTER_NAME="cluster-singlenode"
 OUTPUT_DATA="${OUTPUT_BUCKET}/${CLUSTER_NAME}"  # Dossier de sortie spécifique pour chaque exécution
 
 
-echo "Création du cluster Dataproc $CLUSTER_NAME avec 4 nœud de travail..."
+echo "Création du cluster Dataproc $CLUSTER_NAME avec 1 nœud de travail..."
 gcloud dataproc clusters create $CLUSTER_NAME \
     --region $REGION \
-    --master-machine-type n2-highmem-8 \
-    --master-boot-disk-size 100GB \
-    --num-workers 4 \
-    --worker-machine-type n2-highmem-8 \
-    --worker-boot-disk-size 100GB \
-    --image-version "2.0-debian10" \
+    --single-node \                        # Mode single-node
+    --master-machine-type=n4-standard-4 \  # Type de machine pour le nœud maître (4 vCPU, 16 Go RAM)
+    --worker-machine-type=n4-standard-4 \  # Type de machine pour les nœuds travailleurs (même type de machine)
+    --image-version=2.0-debian10 \         # Version de l'image Dataproc
+    --num-workers=0                       # Pas de nœuds workers, seulement un nœud maître
+    --master-disksize=50GB \
     --project $PROJECT_ID
 
 echo "Exécution du script PySpark RDD PageRank sur le cluster $CLUSTER_NAME ..."
